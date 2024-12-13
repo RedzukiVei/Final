05bcedd0774be76a7fbe1db7589b3c0049293f0e
Juan Alexis Quispe Luque <jquispel@ulasalle.edu.pe>
2024-12-12 19:55:22 -0500

    Configurando AppDatabase para gestionar la base de datos de puntuaciones

--- app/src/main/java/com/example/afinal/data/AppDatabase.kt
+++ app/src/main/java/com/example/afinal/data/AppDatabase.kt
@@ -1,4 +1,32 @@
 package com.example.afinal.data
 
-class AppDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+// Base de datos que incluye la tabla "scores"
+@Database(entities = [Score::class], version = 1, exportSchema = false)
+
+class AppDatabase : RoomDatabase() {
+
+    abstract fun scoreDao(): ScoreDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: AppDatabase? = null
+
+        // Método para obtener una instancia única de la base de datos
+        fun getDatabase(context: Context): AppDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    AppDatabase::class.java,
+                    "scores_database"
+                ).build()
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
 }
