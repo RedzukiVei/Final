dc2bad60171dcc0b8feaa89e5bb3acb6718beed7
Juan Alexis Quispe Luque <jquispel@ulasalle.edu.pe>
2024-12-12 19:52:43 -0500

    ScoreDao con métodos para insertar, consultar y eliminar puntuaciones

--- app/src/main/java/com/example/afinal/data/ScoreDao.kt
+++ app/src/main/java/com/example/afinal/data/ScoreDao.kt
@@ -1,4 +1,22 @@
 package com.example.afinal.data
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
+
+// DAO para realizar operaciones en la tabla "scores"
+@Dao
+
 class ScoreDao {
+    // Insertar una nueva puntuación
+    @Insert
+    suspend fun insert(score: Score)
+
+    // Obtener todas las puntuaciones ordenadas por mayor puntuación
+    @Query("SELECT * FROM scores ORDER BY score DESC")
+    suspend fun getAllScores(): List<Score>
+
+    // Eliminar todas las puntuaciones
+    @Query("DELETE FROM scores")
+    suspend fun deleteAll()
 }
