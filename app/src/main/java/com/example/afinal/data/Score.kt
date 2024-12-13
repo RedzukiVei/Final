b8ed7757fd87acf441a35ab407b22be948e6462f
Juan Alexis Quispe Luque <jquispel@ulasalle.edu.pe>
2024-12-12 19:48:02 -0500

    Definida entidad Score con atributos para jugador, puntuación y fecha

--- app/src/main/java/com/example/afinal/data/Score.kt
+++ app/src/main/java/com/example/afinal/data/Score.kt
@@ -1,4 +1,14 @@
 package com.example.afinal.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+// Entidad para la tabla "scores"
+@Entity(tableName = "scores")
+
 class Score {
+    @PrimaryKey(autoGenerate = true) val id: Int = 0, // ID único para cada puntuación
+    val playerName: String, // Nombre del jugador
+    val score: Int,         // Puntuación obtenida
+    val date: Long          // Fecha de la puntuación (en milisegundos)
 }
